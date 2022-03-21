2262833f6c7d1c9bd3c268e1c24c5d02ff7e997b
Kirill Kopteloy <kopteloy@gmail.com>
2022-03-21 15:24:41 +0300

    feat: added checkbox,styles,js for card;refactor: change card structure,added styles

--- t2.01/src/components/Card/Card.js
+++ t2.01/src/components/Card/Card.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import './Card.css'
 
+
 const Card = (props) => {
+  const [classes, addNewClass] = useState('content__item');
+  const clickHandler = (event) => {
+    event.target.checked === true ? addNewClass('content__item content__item--red') : addNewClass('content__item'); 
+  }
+
   return (
-    <div className="content__item">
-      <h2 className="content__title">{props.title}</h2>
+    <div className={classes}>
+      <div className="content__header">
+        <h2 className="content__title">{props.title}</h2>
+        <input className="content__check" type="checkbox" onClick={clickHandler}></input>
+      </div>
       <p className="content__text">{props.text}</p>
     </div>
   );
