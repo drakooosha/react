3e48abb3fb7fa7691db96c8ff81d956896f6318d
Kirill Kopteloy <kopteloy@gmail.com>
2022-03-22 01:02:34 +0300

    refactor: change js of card

--- t2.01/src/components/Card/Card.js
+++ t2.01/src/components/Card/Card.js
@@ -3,13 +3,13 @@ import './Card.css'
 
 
 const Card = (props) => {
-  const [classes, addNewClass] = useState('content__item');
+  const [checked, setChecked] = useState(false);
   const clickHandler = (event) => {
-    event.target.checked === true ? addNewClass('content__item content__item--red') : addNewClass('content__item'); 
+    setChecked(event.target.checked); 
   }
 
   return (
-    <div className={classes}>
+    <div className={'content__item ' + (checked ? 'content__item--red' : '')}>
       <div className="content__header">
         <h2 className="content__title">{props.title}</h2>
         <input className="content__check" type="checkbox" onClick={clickHandler}></input>
