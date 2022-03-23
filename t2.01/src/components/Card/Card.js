8e0b78fc014b66fb8e5e42c9a859aa11a008c377
Kirill Kopteloy <kopteloy@gmail.com>
2022-03-23 23:21:53 +0300

    feat: added edit button, added css styles

--- t2.01/src/components/Card/Card.js
+++ t2.01/src/components/Card/Card.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { GiCancel,GiCheckMark } from "react-icons/gi";
 import './Card.css'
 
 
@@ -9,10 +11,13 @@ const Card = (props) => {
   return (
     <div className={'content__item ' + (checked ? 'content__item--red' : '')}>
       <div className="content__header">
-        <h2 className="content__title">{props.title}</h2>
+        <h2 className="content__title" contentEditable="true" suppressContentEditableWarning="true">{props.title}</h2>
+        <div className="content__nav">
+        <button className="content__edit"><AiFillEdit /></button>
         <input className="content__check" type="checkbox" onClick={clickHandler}></input>
+        </div>
       </div>
-      <p className="content__text">{props.text}</p>
+      <p className="content__text" contentEditable="true" suppressContentEditableWarning="true">{props.text}</p>
     </div>
   );
 }
