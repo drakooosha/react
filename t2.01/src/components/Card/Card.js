db1671fcfc6bc4cea53eb1527baa80ab635c9a5b
Kirill Kopteloy <kopteloy@gmail.com>
2022-03-25 19:51:36 +0300

    refactor: deleted Cardnav component, changed Card structure, styles

--- t2.01/src/components/Card/Card.js
+++ t2.01/src/components/Card/Card.js
@@ -1,43 +1,81 @@
 import React, { useState } from "react";
 import "./Card.css";
-import CardNav from "./CardNav";
+import { AiFillEdit } from "react-icons/ai";
+import { GiCancel, GiCheckMark } from "react-icons/gi";
 
 const Card = (props) => {
   const [checked, setChecked] = useState(false);
   const [editMode, setEditMode] = useState(false);
-  const [title, setTitle] = useState(props.title);
-  const [text, setText] = useState(props.text);
+  const [title, setTitle] = useState(props.defaultTitle);
+  const [text, setText] = useState(props.defaultText);
 
-  let Newtitle = '';
-  let Newtext = '';
   const clickHandler = () => setChecked(!checked);
 
-  const ModeHandler = () => {
-    if(checked) clickHandler();
+  const editHandler = () => {
+    if(checked) setChecked(!checked); 
     setEditMode(!editMode);
   };
 
-  const onInputTitleHadler = event => {
-    Newtitle = event.target.value;
+  const submitHandler = () => {
+    props.onSubmit(title,text);
+    setEditMode(!editMode);
+  };
+
+  const cancelHandler = () => {
+    setTitle(props.defaultTitle);
+    setText(props.defaultText);
+    setEditMode(!editMode);
+  };
+
+  const titleInputHandler = event => {
+    setTitle(event.target.value);
   }
 
-  const onInputTextHadler = event => {
-    Newtext = event.target.value;
-  }
-
-  const changeContentHandler = () => {
-    setTitle(Newtitle);
-    setText(Newtext);
+  const textInputHandler = event => {
+    setText(event.target.value);
   }
   return (
     <div className={"content__item " + (checked ? "content__item--red" : "")}>
-      <div className="content__header">
-        {(!editMode && <h2 className="content__title"> {title}</h2>)}
-        {(editMode && <input className="content__input content__input--title" type="text" placeholder="Change Title" onChange={onInputTitleHadler}></input>)}
-        <CardNav editMode={editMode} inputСhecked={checked} changeMode={ModeHandler} inputClick={clickHandler} onChangeContent={changeContentHandler}/>
-      </div>
-      {(!editMode && <p className="content__text">{text}</p>)}
-      {(editMode && <input className="content__input content__input--text" type="text" placeholder="Change Text" onChange={onInputTextHadler}></input>)}
+      {editMode ? (
+        <div className="content__header">
+          <input
+            className="content__input content__input--title"
+            type="text"
+            placeholder="Change Title"
+            value={title}
+            onChange={titleInputHandler}
+          ></input>
+          <div className="content__nav">
+            <button className="content__edit" onClick={submitHandler}>
+              <GiCheckMark />
+            </button>
+            <button className="content__edit" onClick={cancelHandler}>
+              <GiCancel />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="content__header">
+          <h2 className="content__title">{title}</h2>
+          <div className="content__nav">
+            <button className="content__edit" onClick={editHandler}>
+              <AiFillEdit />
+            </button>
+            <input className="content__check" type="checkbox" onChange={clickHandler}></input>
+          </div>
+        </div>
+      )}
+      {editMode ? (
+        <input
+          className="content__input content__input--text"
+          type="text"
+          placeholder="Change Text"
+          value={text}
+          onChange={textInputHandler}
+        ></input>
+      ) : (
+        <p className="content__text">{text}</p>
+      )}
     </div>
   );
 };
